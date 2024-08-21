a580688605a33cadc0a6eb1c4d623e0fc1656fcc
feriwnarta <feriwnarta26@gmail.com>
2024-08-21 14:53:24 +0700

    init

---
+++ belajar-oauth2-server/src/main/java/com/feri/belajar_oauth2_server/BelajarOauth2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.feri.belajar_oauth2_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarOauth2ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarOauth2ServerApplication.class, args);
+	}
+
+}
---
+++ belajar-oauth2-server/src/test/java/com/feri/belajar_oauth2_server/BelajarOauth2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feri.belajar_oauth2_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarOauth2ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
